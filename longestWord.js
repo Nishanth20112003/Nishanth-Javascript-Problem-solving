52f6fe569e52c92812d0cd3e9258e59aff6f7e2a
Nishanth20112003 <nishanth20112003@gmail.com>
2025-08-04 16:12:31 +0530

    Solved

---
+++ longestWord.js
@@ -0,0 +1,11 @@
+function findLongestWord(sentence) {
+    const words = sentence.split(' ');
+    let longestWord = '';
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
+console.log(findLongestWord("This is Nishanth"));
